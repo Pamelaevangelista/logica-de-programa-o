1defe685613802f3ff54775b954157e50c46a9df
Pamelaevangelista <panuemanuelecilva@gmail.com>
2024-09-13 21:26:47 -0300

    aula 3 atualizada

---
+++ incremento_decremento/exercicio4.js
@@ -0,0 +1,12 @@
+/*Escreva um programa que usa um loop while para imprimir
+todos os números ímpares entre 1 e 20.*/
+
+let numero = 1;
+
+while (numero <=20){
+    if(numero %2 !== 0){ 
+        console.log(numero)
+    }
+    numero++
+}
+
---
+++ incremento_decremento/exercicio5.js
@@ -0,0 +1,9 @@
+/*Escreva um programa que continua pedindo ao usuário para
+inserir uma senha até que a senha correta seja digitada.*/
+
+let senha = "VouAprender";
+let senhaIncorreta;
+
+while ( senha !== senhaIncorreta ){
+   senhaIncorreta = prompt("Digite a senha")
+}
